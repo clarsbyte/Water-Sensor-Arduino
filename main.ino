6a8f3acd719724a77147921c66a624da4f98db05
clarissa <92129914+clarsbyte@users.noreply.github.com>
2024-05-16 22:16:09 +0700

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,41 @@
+const int analogInPin = A0;
+int sensorValue = 0;
+const int buzzerpin =10;
+
+void setup() {
+  // declare pin  to be an output:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(buzzerpin, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  delay(2);
+  if((sensorValue>=100)&&(sensorValue<=420)){
+    digitalWrite(2,HIGH);
+    delay(100);
+    }
+  else if((sensorValue>=421)&&(sensorValue<=500)){
+   digitalWrite(3,HIGH);
+   delay(100);
+    }  
+  else if((sensorValue>=501)&&(sensorValue<=700)){
+    digitalWrite(4,HIGH);
+    tone(buzzerpin, 100);
+    delay(100);
+    }
+  else{
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    delay(100);
+    noTone(buzzerpin);
+    }
+}
